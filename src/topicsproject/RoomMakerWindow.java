c4265a57f14d5fdb83cd3240812e2b06b0263ecf
Zachary Birnholz <zachary@birnholz.com>
2013-11-21 18:45:11 -0800

    Began work on RoomMakerWindow

--- src/topicsproject/RoomMakerWindow.java
+++ src/topicsproject/RoomMakerWindow.java
@@ -1,8 +1,29 @@
 package topicsproject;
 
-import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class RoomMakerWindow extends JFrame {
-
+	private JPanel panel;
+	
+	public RoomMakerWindow() {
+		super("Room Maker");
+		panel=new JPanel();
+		panel.setLayout(new GridLayout(0, 2));
+		panel.add(new JLabel("Name: "));
+		panel.add(new JTextField());
+		panel.add(new JLabel("Description: "));
+		panel.add(new JTextField());
+		JScrollPane scrollPane=new JScrollPane(panel);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBorder(null);
+		add(scrollPane);
+		setSize(400, 400);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new RoomMakerWindow();
+	}
 }
