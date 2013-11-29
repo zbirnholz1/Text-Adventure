757423402235aea0435359b13ba41a5c6a1776dc
Zachary Birnholz <zachary@birnholz.com>
2013-11-28 21:24:20 -0800

    Added JCheckBoxes for Fields of type boolean

--- src/topicsproject/ObjectPanel.java
+++ src/topicsproject/ObjectPanel.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -26,7 +27,6 @@ import textadventure.TAObject;
 public class ObjectPanel extends JPanel {
 	private Class<?> objectClass;
 	private TAObject instance;
-	private JTextField fieldInFocus;
 
 	private final Map<String, MouseListener> mouseListeners=initializeMouseListeners();
 
@@ -63,11 +63,18 @@ public class ObjectPanel extends JPanel {
 		while(!fields.isEmpty()) {
 			Field f=fields.poll();
 			add(new JLabel(f.getName(), JLabel.CENTER));
-			FieldTextField textField=new FieldTextField(f);
-			textField.addActionListener(new DefaultActionListener());
-			if(mouseListeners.get(f.getName())!=null)
-				textField.addMouseListener(mouseListeners.get(f.getName()));
-			add(textField);
+			if(f.getType().getName().equals("boolean")) {
+				JCheckBox checkBox=new JCheckBox();
+				checkBox.addActionListener(new CheckBoxListener(f));
+				add(checkBox);
+			}
+			else {
+				FieldTextField textField=new FieldTextField(f);
+				textField.addActionListener(new DefaultActionListener());
+				if(mouseListeners.get(f.getName())!=null)
+					textField.addMouseListener(mouseListeners.get(f.getName()));
+				add(textField);
+			}
 		}
 	}
 
@@ -86,12 +93,78 @@ public class ObjectPanel extends JPanel {
 			}
 			public void mouseReleased(MouseEvent arg0) {
 			}
-			
+
 		});
 		//TODO add special listeners for fields like description, etc.
 		return map;
 	}
-	
+
+	private void setBasicField(Field field, String text, Exception e1) {
+		String methodName=Character.toUpperCase(field.getName().charAt(0))+field.getName().substring(1);
+		Class<?> c=objectClass;
+		Class<?> type=field.getType();
+		while(!c.getName().equals("java.lang.Object")) {
+			try {
+				Method method=c.getDeclaredMethod("set"+methodName, new Class<?>[]{type});
+				if(type.getName().equals("boolean"))
+					method.invoke(instance, new Object[]{Boolean.parseBoolean(text)});
+				else if(type.getName().equals("int"))
+					method.invoke(instance, new Object[]{Integer.parseInt(text)});
+				else if(type.getName().equals("java.lang.String"))
+					method.invoke(instance, new Object[]{text});
+				else
+					System.out.println("For some reason the field for "+text+" of type "+type.getName()+" was not set.");
+			} catch (Exception e2) {
+				//e2.printStackTrace();
+				c=c.getSuperclass();
+				continue;
+			}
+			break;
+		}
+		try {
+			if(text.equalsIgnoreCase(""+field.get(instance)))
+				System.out.println("Unless it says otherwise above, "+field.getName()+" has been assigned the value of "+text);
+			else {
+				if(e1!=null)
+					e1.printStackTrace();
+				else
+					System.out.println("There was an error setting "+field+" with the value "+text);
+			}
+		} catch(Exception e2) {
+			Method getMethod=null;
+			String getMethodName="get"+methodName;
+			if(type.getName().equals("boolean"))
+				getMethodName="is"+methodName;
+			c=objectClass;
+			while(!c.getName().equals("java.lang.Object")) {
+				try {
+					getMethod=c.getDeclaredMethod(getMethodName, new Class<?>[]{});
+				} catch (Exception e3) {
+					//e2.printStackTrace();
+					c=c.getSuperclass();
+					continue;
+				}
+				break;
+			}
+			try {
+				if(text.equalsIgnoreCase(""+getMethod.invoke(instance, new Object[]{})))
+					System.out.println("Unless it says otherwise above, "+field.getName()+" has been assigned the value of "+text);
+				else {
+					if(e1!=null)
+						e1.printStackTrace();
+					else 
+						System.out.println("Error! Field: "+field+", Value: "+text);
+				}
+			} catch(Exception e3) {
+				if(e1!=null)
+					e1.printStackTrace();
+				else 
+					System.out.println("Error! Field: "+field+", Value: "+text);
+				e3.printStackTrace();
+			}
+		}
+	}
+
 	class FieldTextField extends JTextField {
 		private Field field;
 
@@ -103,6 +176,24 @@ public class ObjectPanel extends JPanel {
 		public Field getField() {
 			return field;
 		}
+	}
+
+	class CheckBoxListener implements ActionListener {
+		private Field field;
+
+		public CheckBoxListener(Field f) {
+			field=f;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			try {
+				String value=new Boolean(((JCheckBox)e.getSource()).isSelected()).toString();
+				setBasicField(field, value, null);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
 	}
 
 	class DefaultActionListener implements ActionListener {
@@ -130,57 +221,7 @@ public class ObjectPanel extends JPanel {
 				JOptionPane.showMessageDialog(ObjectPanel.this, "Please enter an int value for "+source.getField().getName());
 				return;
 			} catch(IllegalAccessException e1) {
-				String methodName=Character.toUpperCase(source.getField().getName().charAt(0))+source.getField().getName().substring(1);
-				Class<?> c=objectClass;
-				while(!c.getName().equals("java.lang.Object")) {
-					try {
-						Method method=c.getDeclaredMethod("set"+methodName, new Class<?>[]{type});
-						if(type.getName().equals("boolean"))
-							method.invoke(instance, new Object[]{Boolean.parseBoolean(text)});
-						else if(type.getName().equals("int"))
-							method.invoke(instance, new Object[]{Integer.parseInt(text)});
-						else if(type.getName().equals("java.lang.String"))
-							method.invoke(instance, new Object[]{text});
-						else
-							System.out.println("For some reason the field for "+text+" of type "+type.getName()+" was not set.");
-					} catch (Exception e2) {
-						//e2.printStackTrace();
-						c=c.getSuperclass();
-						continue;
-					}
-					break;
-				}
-				try {
-					if(text.equalsIgnoreCase(""+source.getField().get(instance)))
-						System.out.println("Unless it says otherwise above, "+source.getField().getName()+" has been assigned the value of "+text);
-					else
-						e1.printStackTrace();
-				} catch(Exception e2) {
-					Method getMethod=null;
-					String getMethodName="get"+methodName;
-					if(type.getName().equals("boolean"))
-						getMethodName="is"+methodName;
-					c=objectClass;
-					while(!c.getName().equals("java.lang.Object")) {
-						try {
-							getMethod=c.getDeclaredMethod(getMethodName, new Class<?>[]{});
-						} catch (Exception e3) {
-							//e2.printStackTrace();
-							c=c.getSuperclass();
-							continue;
-						}
-						break;
-					}
-					try {
-						if(text.equalsIgnoreCase(""+getMethod.invoke(instance, new Object[]{})))
-							System.out.println("Unless it says otherwise above, "+source.getField().getName()+" has been assigned the value of "+text);
-						else
-							e1.printStackTrace();
-					} catch(Exception e3) {
-						e1.printStackTrace();
-						e3.printStackTrace();
-					}
-				}
+				setBasicField(source.getField(), text, e1);
 			}
 		}
 	}
